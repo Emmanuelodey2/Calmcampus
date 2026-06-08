4489efa52116e6d5e9eb7f449481e3e306b10f7c
Emmanuelodey2 <116392410+Emmanuelodey2@users.noreply.github.com>
2026-06-08 17:08:33 +0100

    Make Page component async and modify return value
    Updated Page component to be asynchronous and handle token validation.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,11 @@
-import DashboardPage from "./dash";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
   const cookieStore = await cookies();
   const token = cookieStore.get("access")?.value;
 
   if (!token) redirect("/login");
 
-  return <DashboardPage />;
+  return <div>Dashboard</div>;
 }
