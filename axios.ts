e24cc2d8f99741ece99c769285c20f2711796b83
Emmanuelodey2 <116392410+Emmanuelodey2@users.noreply.github.com>
2026-06-13 02:36:28 +0100

    Change API base URL to production server

--- axios.ts
+++ axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiRequest = axios.create({
-  baseURL: 'http://localhost:8000/api', // Your Django server URL
+  baseURL: 'https://calmcampus-5hry.onrender.com/', // Your Django server URL
   withCredentials: true,                // Automatically sends cookies with every request
 });
 
